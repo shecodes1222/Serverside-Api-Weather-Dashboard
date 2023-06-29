ffc70b76e0ada02105a760bddc119931c7dabdae
Jasmina <122895875+shecodes1222@users.noreply.github.com>
2023-06-29 14:51:14 -0400

    cards and search history button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
-var key = '64f2ee2a8261daa4d9f780f5b365f275';
-var city = "Miami"
+var cities = JSON.parse(localStorage.getItem('search-history')) || [];
+var addCity = function (city) {
+    console.log("add city");
+    var APIKey = "517b598cb370b4d60b6492926681f7ac";
+    console.log(city);
+    var cityUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&limit=5&appid=" + APIKey;
 
+
+    fetch(cityUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log("fetched city", data);
+            populateCityToday(data);
+            populateForecast(data);
+        });
+    saveCity(city);
+};
 
 //Retrieves current date and time
 var date = moment().format('dddd, MMMM Do YYYY');
