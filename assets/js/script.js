06ca53ff8bcbef2cb209039945724e7edc1968ee
Jasmina <122895875+shecodes1222@users.noreply.github.com>
2023-06-29 12:48:11 -0400

    added weather data to the today card from api weather map

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,42 +9,102 @@ var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
 var cityHist = [];
 //Following will save the text value as an  array and store it in local storage.
 $('.search').on("click", function (event) {
-	event.preventDefault();
-	city = $(this).parent('.btnPar').siblings('.textVal').val().trim();
-	if (city === "") {
-		return;
-	};
-	cityHist.push(city);
+    event.preventDefault();
+    city = $(this).parent('.btnPar').siblings('.textVal').val().trim();
+    if (city === "") {
+        return;
+    };
+    cityHist.push(city);
 
-	localStorage.setItem('city', JSON.stringify(cityHist));
-	fiveForecastEl.empty();
-	getHistory();
-	getWeatherToday();
+    localStorage.setItem('city', JSON.stringify(cityHist));
+    fiveForecastEl.empty();
+    getHistory();
+    getWeatherToday();
 });
 //Following will create buttons based on search history 
 var contHistEl = $('.cityHist');
 function getHistory() {
-	contHistEl.empty();
+    contHistEl.empty();
 
-	for (let i = 0; i < cityHist.length; i++) {
+    for (let i = 0; i < cityHist.length; i++) {
 
-		var rowEl = $('<row>');
-		var btnEl = $('<button>').text(`${cityHist[i]}`)
+        var rowEl = $('<row>');
+        var btnEl = $('<button>').text(`${cityHist[i]}`)
 
-		rowEl.addClass('row histBtnRow');
-		btnEl.addClass('btn btn-outline-secondary histBtn');
-		btnEl.attr('type', 'button');
+        rowEl.addClass('row histBtnRow');
+        btnEl.addClass('btn btn-outline-secondary histBtn');
+        btnEl.attr('type', 'button');
 
-		contHistEl.prepend(rowEl);
-		rowEl.append(btnEl);
-	} if (!city) {
-		return;
-	}
+        contHistEl.prepend(rowEl);
+        rowEl.append(btnEl);
+    } if (!city) {
+        return;
+    }
     //Following will allow the buttons to start a search as well
-	$('.histBtn').on("click", function (event) {
-		event.preventDefault();
-		city = $(this).text();
-		fiveForecastEl.empty();
-		getWeatherToday();
-	});
+    $('.histBtn').on("click", function (event) {
+        event.preventDefault();
+        city = $(this).text();
+        fiveForecastEl.empty();
+        getWeatherToday();
+    });
+};
+//Grab the main 'Today' card body.
+var cardTodayBody = $('.cardBodyToday')
+//Applies the weather data to the today card and then launches the five day forecast
+function getWeatherToday() {
+    var getUrlCurrent = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`;
+
+    $(cardTodayBody).empty();
+
+    $.ajax({
+        url: getUrlCurrent,
+        method: 'GET',
+    }).then(function (response) {
+        $('.cardTodayCityName').text(response.name);
+        $('.cardTodayDate').text(date);
+        //Icons
+        $('.icons').attr('src', `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`);
+        // Temperature
+        var pEl = $('<p>').text(`Temperature: ${response.main.temp} °F`);
+        cardTodayBody.append(pEl);
+        //Feels Like
+        var pElTemp = $('<p>').text(`Feels Like: ${response.main.feels_like} °F`);
+        cardTodayBody.append(pElTemp);
+        //Humidity
+        var pElHumid = $('<p>').text(`Humidity: ${response.main.humidity} %`);
+        cardTodayBody.append(pElHumid);
+        //Wind Speed
+        var pElWind = $('<p>').text(`Wind Speed: ${response.wind.speed} MPH`);
+        cardTodayBody.append(pElWind);
+        //Set the lat and long from the searched city
+        var cityLon = response.coord.lon;
+        // console.log(cityLon);
+        var cityLat = response.coord.lat;
+        // console.log(cityLat);
+
+        var getUrlUvi = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=hourly,daily,minutely&appid=${key}`;
+        $.ajax({
+            url: getUrlUvi,
+            method: 'GET',
+        }).then(function (response) {
+            var pElUvi = $('<p>').text(`UV Index: `);
+            var uviSpan = $('<span>').text(response.current.uvi);
+            var uvi = response.current.uvi;
+            pElUvi.append(uviSpan);
+            cardTodayBody.append(pElUvi);
+            //set the UV index to match an exposure chart severity based on color 
+            if (uvi >= 0 && uvi <= 2) {
+                uviSpan.attr('class', 'green');
+            } else if (uvi > 2 && uvi <= 5) {
+                uviSpan.attr("class", "yellow")
+            } else if (uvi > 5 && uvi <= 7) {
+                uviSpan.attr("class", "orange")
+            } else if (uvi > 7 && uvi <= 10) {
+                uviSpan.attr("class", "red")
+            } else {
+                uviSpan.attr("class", "purple")
+            }
+        });
+    });
+    getFiveDayForecast();
 };
