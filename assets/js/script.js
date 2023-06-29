49858fe6c86daa1dea41d934b7e6885fdf0c0c2d
Jasmina <122895875+shecodes1222@users.noreply.github.com>
2023-06-29 12:29:55 -0400

    added variables, functions to grab current time and date as well as saving value for each search into an array

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var key = '64f2ee2a8261daa4d9f780f5b365f275';
+var city = "Miami"
+
+
+//Grabs the current time and date
+var date = moment().format('dddd, MMMM Do YYYY');
+var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
+
+var cityHist = [];
+//Will save the text value of the search and save it to an array and storage
+$('.search').on("click", function (event) {
+	event.preventDefault();
+	city = $(this).parent('.btnPar').siblings('.textVal').val().trim();
+	if (city === "") {
+		return;
+	};
+	cityHist.push(city);
+
+	localStorage.setItem('city', JSON.stringify(cityHist));
+	fiveForecastEl.empty();
+	getHistory();
+	getWeatherToday();
+});
